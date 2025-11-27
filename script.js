6e876db7dca09dfc2559efb7cf7870e31559ce9e
Leo <leozinho17silvacosta@gmail.com>
2025-11-26 22:41:36 -0800

    Complete JQuery Exercise (accordion & ajax) - WEB 2

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-$(function() {
-  // your code here
-})
+
+// Exercise 1:
+
+$(function () {
+  $('.accordion-header').on('click', function () {
+    $('.accordion-content').slideUp();
+    $(this).next('.accordion-content').slideDown();
+  });
+});
+
+// Exercise 2:
+
+$(function () {
+  $('.todos button').on('click', function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: "GET",
+      success: function(response) {
+        const list = $('.todos ul');
+        list.empty();
+        
+        response.todos.forEach(todo => {
+          list.append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function(err) {
+        console.error(err);
+      }
+    });
+  });
+});
